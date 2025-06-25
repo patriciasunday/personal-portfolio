e4d4a71a0a25c78f3c2f64117d2a7cbbeac87bee
Patricia Sunday <sund0058@algonquinlive.com>
2025-06-24 22:47:01 -0400

    feat(style): Integrate Tailwind into contactme.php for site redesign
    Included custom styles in input.css for use in contactme.php. Removed element classes that were used in the now-deleted contactme stylesheet.
    Replaced those classes with Tailwind utility classes for basic page theme. Primarily focused on input styling classes. Must revisit
    todo comments in contactme.php once the entire site is generally complete.

--- pages/contactme.php
+++ pages/contactme.php
@@ -1,9 +1,10 @@
 <!--
 Code written by Patricia Sunday
-Purpose: this is the contact page for my portfolio
+Purpose: this is the contact form page for my portfolio
+Last Edited: June 24th, 2025
 -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-[100%]">
     <head>
         <title>Contact Me</title>
         <meta charset="UTF-8">
@@ -12,35 +13,57 @@ Purpose: this is the contact page for my portfolio
         <meta name="keywords" content="connect, portfolio, contact, contact me, message, email, form">
         <link rel="stylesheet" href="../css/output.css">
     </head>
-
-    <body>
-        <header>
-            <?php require "nav.php"; ?>
+    <body class="flex flex-col h-[100%] font-[Francois One] text-[var(--text-color)]">
+        <header class="m-8 flex flex-col gap-3">
+            <div class="self-center"><?php require "nav.php"; ?></div>
+            <div class="flex flex-row justify-between mt-5">
+                <h1 class="text-xl font-bold">Pin Your Message!</h1>
+                <input type="submit" form="contact" class="bg-[var(--primary-contrast)] text-[var(--bg-color)] font-bold px-10 rounded-xl active:bg-[var(--text-color)]">
+            </div>
+            <hr>
         </header>
-        <main>
-            <!--contact form, for name, email and message-->
-            <form>
-                <div class="input-row">
-                    <div class="input-combo">
+        <main class="grow m-8 mt-0">
+            <!--contact form (name, email, message)-->
+            <form id="contact" method="GET" class="flex flex-row gap-6" ><!--todo change method once JS is configured-->
+                <div id="file-upload" class="bg-[var(--primary-color)] rounded-4xl min-w-[45%] input-group-flex justify-center items-center">
+                    <label for="file"> 
+                        <img src="../images/fileupload.png" alt="File upload symbol" class="w-[5rem] m-auto">
+                        Choose a file to upload
+                    </label>
+                    <input type="file" id="file" form="contact" class="hidden">
+                </div>
+                <div class="input-group-flex min-w-[50%] gap-5">
+                    <div class="input-group-flex">
                         <label for="name">Name</label>
-                        <input type="text" id="name" name="name" required>
+                        <input type="text" id="name" name="name" placeholder="Enter your name" class="input-border" required>
                     </div>
-                    <div class="input-combo">
+                    <div class="input-group-flex">
                         <label for="email">Email</label>
-                        <input type="email" id="email" name="email" required>
+                        <input type="email" id="email" name="email" placeholder="Enter your email address" class="input-border" required>
                     </div>
-                </div>
-                <div class="message-combo">
-                    <label for="message">Message</label>
-                    <textarea id="message" name="message" required></textarea>
-                </div>
-                <div class="end-bar">
-                    <input type="submit">
-                    <input type="reset">
+                    <div class="input-group-flex">
+                        <label for="subject">Subject</label>
+                        <select id="subject" name="subject" class="input-border" required>
+                           <div class="inline"><img src="../images/algonquin-b-building.jpg"> <option>Inquiry</option></div>
+                            <option>Feedback</option>
+                            <option>Offer</option>
+                            <option>Freelancing</option>
+                        </select>
+                    </div>
+                    <div class="input-group-flex">
+                        <label for="message">Message</label>
+                        <textarea id="message" name="message" placeholder="Enter your message" class="input-border min-h-[200px]" required></textarea>
+                    </div>
                 </div>
             </form>
         </main>
-
         <?php require "footer.php"; ?>
     </body>
 </html>
+<!--
+todo: remove required attrib from inputs & use js or php
+todo: use universal variable for font for all pages
+todo: fix file upload so that clicking on any part of the div prompts upload
+todo: fix file upload so that the label changes to their filename when they put in file
+todo: replace arbitrary custom values
+-->
