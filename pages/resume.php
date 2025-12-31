d4a3f5d663af4c8e561e7637750a7b01a55178a8
Patricia Sunday <patriciasnsunday@outlook.com>
2025-12-30 22:53:20 -0500

    chore(resume): Slightly edit content and utility values
    Completed TODOs by updating text content, replacing resume file, and removing an unnecessary utility class.

--- pages/resume.php
+++ pages/resume.php
@@ -13,10 +13,10 @@ Purpose: this is the resume page for my portfolio
         <meta name="keywords" content="resume, cv, portfolio, programming">
         <link rel="stylesheet" href="/styles/output.css">
     </head>
-    <body class="h-full flex flex-col font-[Francois One]">
+    <body class="h-full flex flex-col">
         <header class="mt-8 flex flex-col items-center"> 
             <?php require "nav.php";?> 
-            <p class="mt-5 italic">Click my name to download my full resume</p>
+            <p class="mt-5 italic">Click my name to download the full resume</p>
         </header>
         <main class="grow m-8 p-8 flex flex-col border-2 border-[var(--text-color)] rounded-xl shadow-xl">
             <div class="self-center flex flex-col">
@@ -27,10 +27,10 @@ Purpose: this is the resume page for my portfolio
                 <p class="self-center">patriciasnsunday@outlook.com</p>
             </div>
             <p class="mt-4">
-                I'm a 3rd-term student with a strong interest in application and web development.
-                I am available for a  4-8 month co-op starting September 2025, and looking forward to 
+                I'm a 4th-term student with a strong interest in application and web development.
+                I am available for a 4 month internship starting Mid-May 2026, and looking forward to 
                 growing my technical and soft skills in a professional setting!
-            </p> <!--todo:edit resume summary statement-->
+            </p> 
             <div class="md:flex md:gap-10"> <!--for responsive layout-->
                 <div class="mt-4">
                     <p class="font-bold">Computer Programming and Analysis</p>
@@ -55,8 +55,3 @@ Purpose: this is the resume page for my portfolio
         <?php require "footer.php"; ?>
     </body>
 </html>
-
-<!--todo: rename images folder to resources or media since it now has resume pdf too-->
-<!--todo: change header to mt-8 instead of m-8 in other pages as well-->
-<!--todo: change h-[100%] to h-full in other pages as well-->
-<!--todo: change hyperlink for email icon in footer to use my non-school email-->
