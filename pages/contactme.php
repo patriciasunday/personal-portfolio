206870fc3348a1d5da137debd14cc60f6301ea1e
Patricia Sunday <patriciasnsunday@outlook.com>
2025-12-30 22:25:02 -0500

    fix(contact): Fix content sizing and replace arbitrary utility values
    Fix content sizing and spacing to improve layout aesthetics. Improve Tailwind use by replacing arbitrary utility class values with Tailwind values.

--- pages/contactme.php
+++ pages/contactme.php
@@ -1,10 +1,11 @@
 <!--
 Code written by Patricia Sunday
 Purpose: this is the contact form page for my portfolio
-Last Edited: June 24th, 2025
+Last Edited: December 30th, 2025
 -->
 <!DOCTYPE html>
-<html lang="en" class="h-[100%]">
+<!--using h-full to allow child <body> element to span full screen height-->
+<html lang="en" class="h-full">
     <head>
         <title>Contact Me</title>
         <meta charset="UTF-8">
@@ -13,46 +14,51 @@ Last Edited: June 24th, 2025
         <meta name="keywords" content="connect, portfolio, contact, contact me, message, email, form">
         <link rel="stylesheet" href="/styles/output.css">
     </head>
-    <body class="flex flex-col h-[100%] font-[Francois One] text-[var(--text-color)]">
-        <header class="m-8 flex flex-col gap-3">
+    <body class=" h-full flex flex-col text-[var(--text-color)]">
+        <header class="m-8 flex flex-col gap-6">
             <div class="self-center"><?php require "nav.php"; ?></div>
-            <div class="flex flex-row justify-between mt-5">
-                <h1 class="text-xl font-bold">Pin Your Message!</h1>
-                <input type="submit" form="contact" class="bg-[var(--primary-contrast)] text-[var(--bg-color)] font-bold px-10 rounded-xl active:bg-[var(--text-color)]">
+            <!--form title header group-->
+            <div class="flex flex-col gap-3">
+                <div class="flex flex-row justify-between mt-5">
+                    <h1 class="text-xl font-bold">Pin Your Message!</h1>
+                    <input type="submit" form="contact" class="px-10 rounded-xl bg-[var(--primary-contrast)] text-[var(--bg-color)] font-bold active:bg-[var(--text-color)]">
+                </div>
+                <hr>
             </div>
-            <hr>
         </header>
-        <main class="grow m-8 mt-0">
+        <main class="m-8 mt-0 grow">
             <!--contact form (name, email, message)-->
-            <form id="contact" method="GET" action="mailto:patriciasnsunday@outlook.com" class="flex flex-row gap-6" ><!--todo: change method and action once php form handling is configured-->
-                <div id="file-upload" class="bg-[var(--primary-color)] rounded-4xl min-w-[45%] input-group-flex justify-center items-center">
+            <form id="contact" method="GET" action="mailto:patriciasnsunday@outlook.com" class="flex flex-col md:flex-row gap-6" >
+                <div id="file-upload" class="input-flex bg-[var(--primary-color)] min-w-100 rounded-4xl justify-center items-center py-4">
                     <label for="file"> 
-                        <img src="../media/fileupload.png" alt="File upload symbol" class="w-[5rem] m-auto">
+                        <img src="../media/fileupload.png" alt="File upload symbol" class="w-10 m-auto">
                         Choose a file to upload
                     </label>
                     <input type="file" id="file" form="contact" class="hidden">
                 </div>
-                <div class="input-group-flex min-w-[50%] gap-5">
-                    <div class="input-group-flex">
+                <!--form text input section-->
+                <!--flex-grow to expand into available page width-->
+                <div class="input-flex gap-5 grow">
+                    <div class="input-flex">
                         <label for="name">Name</label>
                         <input type="text" id="name" name="name" placeholder="Enter your name" class="input-border" required>
                     </div>
-                    <div class="input-group-flex">
+                    <div class="input-flex">
                         <label for="email">Email</label>
                         <input type="email" id="email" name="email" placeholder="Enter your email address" class="input-border" required>
                     </div>
-                    <div class="input-group-flex">
+                    <div class="input-flex">
                         <label for="subject">Subject</label>
                         <select id="subject" name="subject" class="input-border" required>
-                           <div class="inline"><img src="../images/algonquin-b-building.jpg"> <option>Inquiry</option></div>
+                            <option>Inquiry</option>
                             <option>Feedback</option>
                             <option>Offer</option>
                             <option>Freelancing</option>
                         </select>
                     </div>
-                    <div class="input-group-flex">
+                    <div class="input-flex">
                         <label for="message">Message</label>
-                        <textarea id="message" name="message" placeholder="Enter your message" class="input-border min-h-[200px]" required></textarea>
+                        <textarea id="message" name="message" placeholder="Enter your message" class="input-border min-h-40" required></textarea>
                     </div>
                 </div>
             </form>
@@ -61,9 +67,8 @@ Last Edited: June 24th, 2025
     </body>
 </html>
 <!--
-todo: remove required attrib from inputs & use js or php
-todo: use universal variable for font for all pages
 todo: fix file upload so that clicking on any part of the div prompts upload
 todo: fix file upload so that the label changes to their filename when they put in file
-todo: replace arbitrary custom values
+todo: use js to fix mailto mechanism & have form body in the generated email
+todo: change method, action, and remove required attributes once php form handling is configured
 -->
